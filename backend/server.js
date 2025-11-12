fc21e2cf1bc1f548f508ed2308e410b9c3f6b16a
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2025-11-12 08:51:31 +0800

    Updated structure

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,9 @@ dbConnect()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, "frontend", "dist")))
+	app.use(express.static(path.join(__dirname, "..", "frontend", "dist")))
 	app.get(/(.*)/, (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+		res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"))
 	})
 }
 
